dd98b2e60709f6bed3d13ba758d45a556119111c
Hajer Thabet <63084962+Hajer-Thabet@users.noreply.github.com>
2022-01-10 23:23:15 -0500

    Update Time.js

--- src/pomodoro/Time.js
+++ src/pomodoro/Time.js
@@ -15,10 +15,8 @@ const Time = ({
 
   return (
     <div>
-      {/* TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
       <div className="row mb-2">
         <div className="col">
-          {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
           <h2 data-testid="session-title">
             {session?.label} for{' '}
             {session?.label === 'Focusing'
@@ -26,7 +24,6 @@ const Time = ({
               : minutesToDuration(breakDuration)}{' '}
             minutes
           </h2>
-          {/* TODO: Update message below correctly format the time remaining in the current session */}
           <p className="lead" data-testid="session-sub-title">
             {secondsToDuration(session?.timeRemaining)} remaining
           </p>
@@ -41,7 +38,7 @@ const Time = ({
               role="progressbar"
               aria-valuemin="0"
               aria-valuemax="100"
-              aria-valuenow={percent} // TODO: Increase aria-valuenow as elapsed time increases
+              aria-valuenow={percent} // Increase aria-valuenow as elapsed time increases
               style={{ width: `${percent}%` }} // TODO: Increase width % as elapsed time increases
             />
           </div>
