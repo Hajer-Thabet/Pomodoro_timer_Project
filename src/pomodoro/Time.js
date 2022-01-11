5835a3703e94ddd8f71b5a59e15a8d1674fb1060
Hajer Thabet <63084962+Hajer-Thabet@users.noreply.github.com>
2022-01-10 23:37:59 -0500

    Update Time.js

--- src/pomodoro/Time.js
+++ src/pomodoro/Time.js
@@ -42,7 +42,7 @@ const Time = ({
                 aria-valuemin="0"
                 aria-valuemax="100"
                 aria-valuenow={percent} // Increase aria-valuenow as elapsed time increases
-                style={{ width: `${percent}%` }} // TODO: Increase width % as elapsed time increases
+                style={{ width: `${percent}%` }} // Increase width % as elapsed time increases
               />
             </div>
           </div>
