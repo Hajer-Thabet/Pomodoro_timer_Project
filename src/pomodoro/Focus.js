d0e37838c3bf38c98c7d2a5da1dcffdd6a5e66a0
Hajer Thabet <63084962+Hajer-Thabet@users.noreply.github.com>
2022-01-10 23:24:10 -0500

    Update Focus.js

--- src/pomodoro/Focus.js
+++ src/pomodoro/Focus.js
@@ -19,11 +19,11 @@ const Focus = ({ session, focusDuration, setFocusDuration }) => {
     <div className="Focus col">
       <div className="input-group input-group-lg mb-2">
         <span className="input-group-text" data-testid="duration-focus">
-          {/* TODO: Update this text to display the current focus session duration */}
+          {/* Update this text to display the current focus session duration */}
           Focus Duration: {minutesToDuration(focusDuration)}
         </span>
         <div className="input-group-append">
-          {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
+          {/* Implement decreasing focus duration and disable during a focus or break session */}
           <button
             type="button"
             className="btn btn-secondary"
@@ -33,7 +33,7 @@ const Focus = ({ session, focusDuration, setFocusDuration }) => {
           >
             <span className="oi oi-minus" />
           </button>
-          {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
+          {/* Implement increasing focus duration  and disable during a focus or break session */}
           <button
             type="button"
             className="btn btn-secondary"
