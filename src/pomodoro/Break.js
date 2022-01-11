450fa2aa2ebb3ae319031718fc896eac65af81b7
Hajer Thabet <63084962+Hajer-Thabet@users.noreply.github.com>
2022-01-10 23:22:12 -0500

    Update Break.js

--- src/pomodoro/Break.js
+++ src/pomodoro/Break.js
@@ -20,11 +20,9 @@ const Break = ({ session, breakDuration, setBreakDuration }) => {
       <div className="float-right">
         <div className="input-group input-group-lg mb-2">
           <span className="input-group-text" data-testid="duration-break">
-            {/* TODO: Update this text to display the current break session duration */}
             Break Duration: {minutesToDuration(breakDuration)}
           </span>
           <div className="input-group-append">
-            {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
             <button
               type="button"
               className="btn btn-secondary"
@@ -34,7 +32,6 @@ const Break = ({ session, breakDuration, setBreakDuration }) => {
             >
               <span className="oi oi-minus" />
             </button>
-            {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
             <button
               type="button"
               className="btn btn-secondary"
