79fee87fcb4e20f4d5bb3b01bb2a73879c7625d2
Hajer Thabet <63084962+Hajer-Thabet@users.noreply.github.com>
2022-01-10 23:37:19 -0500

    Update Time.js

--- src/pomodoro/Time.js
+++ src/pomodoro/Time.js
@@ -13,39 +13,45 @@ const Time = ({
   let elapsedTime = totalDuration - session?.timeRemaining;
   let percent = (elapsedTime / totalDuration) * 100;
 
-  return (
-    <div>
-      <div className="row mb-2">
-        <div className="col">
-          <h2 data-testid="session-title">
-            {session?.label} for{' '}
-            {session?.label === 'Focusing'
-              ? minutesToDuration(focusDuration)
-              : minutesToDuration(breakDuration)}{' '}
-            minutes
-          </h2>
-          <p className="lead" data-testid="session-sub-title">
-            {secondsToDuration(session?.timeRemaining)} remaining
-          </p>
-          <h3>{isTimerRunning ? null : 'Paused'}</h3>
+  if(session){
+    return (
+      <div>
+        {/* This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
+        <div className="row mb-2">
+          <div className="col">
+            {/* Message below to include current session (Focusing or On Break) total duration */}
+            <h2 data-testid="session-title">
+              {session?.label} for{' '}
+              {session?.label === 'Focusing'
+                ? minutesToDuration(focusDuration)
+                : minutesToDuration(breakDuration)}{' '}
+              minutes
+            </h2>
+            <p className="lead" data-testid="session-sub-title">
+              {secondsToDuration(session?.timeRemaining)} remaining
+            </p>
+            <h3>{isTimerRunning ? null : 'Paused'}</h3>
+          </div>
         </div>
-      </div>
-      <div className="row mb-2">
-        <div className="col">
-          <div className="progress" style={{ height: '20px' }}>
-            <div
-              className="progress-bar"
-              role="progressbar"
-              aria-valuemin="0"
-              aria-valuemax="100"
-              aria-valuenow={percent} // Increase aria-valuenow as elapsed time increases
-              style={{ width: `${percent}%` }} // TODO: Increase width % as elapsed time increases
-            />
+        <div className="row mb-2">
+          <div className="col">
+            <div className="progress" style={{ height: '20px' }}>
+              <div
+                className="progress-bar"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                aria-valuenow={percent} // Increase aria-valuenow as elapsed time increases
+                style={{ width: `${percent}%` }} // TODO: Increase width % as elapsed time increases
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Time;
